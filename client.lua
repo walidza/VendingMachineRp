69db164655da9b28c2fbbcff670638123b0e3469
walidza <47478357+walidza@users.noreply.github.com>
2019-09-08 00:48:29 +0200

    Add files via upload

---
+++ client.lua
@@ -0,0 +1,197 @@
+
+addEventHandler("onClientResourceStart", resourceRoot,
+    function  ()
+        b1 = guiCreateButton(429, 272, 136, 123, "", false)
+        guiSetAlpha(b1, 0.00)
+        guiSetProperty(b1, "NormalTextColour", "FFAAAAAA")
+
+
+        b2 = guiCreateButton(600, 273, 136, 123, "", false)
+        guiSetAlpha(b2, 0.00)
+        guiSetProperty(b2, "NormalTextColour", "FFAAAAAA")
+
+
+        b3 = guiCreateButton(772, 273, 136, 123, "", false)
+        guiSetAlpha(b3, 0.00)
+        guiSetProperty(b3, "NormalTextColour", "FFAAAAAA")
+
+
+        b4 = guiCreateButton(430, 431, 136, 123, "", false)
+        guiSetAlpha(b4, 0.00)
+        guiSetProperty(b4, "NormalTextColour", "FFAAAAAA")
+
+
+        b5 = guiCreateButton(600, 429, 136, 123, "", false)
+        guiSetAlpha(b5, 0.00)
+        guiSetProperty(b5, "NormalTextColour", "FFAAAAAA")
+
+
+        b6 = guiCreateButton(772, 431, 136, 123, "", false)
+        guiSetAlpha(b6, 0.00)
+        guiSetProperty(b6, "NormalTextColour", "FFAAAAAA")
+
+
+        --buy = guiCreateButton(569, 598, 203, 83, "", false)
+        --guiSetAlpha(buy, 0.00)
+        --guiSetProperty(buy, "NormalTextColour", "FFAAAAAA")
+
+
+        Close = guiCreateButton(888, 205, 20, 19, "", false)
+        guiSetAlpha(Close, 0.00)
+        guiSetProperty(Close, "NormalTextColour", "FFAAAAAA")
+    end
+)
+
+
+    function dxbutton()
+        dxDrawLine(774 - 1, 431 - 1, 774 - 1, 553, tocolor(62, 62, 62, 108), 1, false)
+        dxDrawLine(911, 431 - 1, 774 - 1, 431 - 1, tocolor(62, 62, 62, 108), 1, false)
+        dxDrawLine(774 - 1, 553, 911, 553, tocolor(62, 62, 62, 108), 1, false)
+        dxDrawLine(911, 553, 911, 431 - 1, tocolor(62, 62, 62, 108), 1, false)
+        dxDrawRectangle(774, 431, 137, 122, tocolor(85, 85, 85, 90), false)
+        dxDrawLine(599 - 1, 430 - 1, 599 - 1, 552, tocolor(62, 62, 62, 108), 1, false)
+        dxDrawLine(736, 430 - 1, 599 - 1, 430 - 1, tocolor(62, 62, 62, 108), 1, false)
+        dxDrawLine(599 - 1, 552, 736, 552, tocolor(62, 62, 62, 108), 1, false)
+        dxDrawLine(736, 552, 736, 430 - 1, tocolor(62, 62, 62, 108), 1, false)
+        dxDrawRectangle(599, 430, 137, 122, tocolor(85, 85, 85, 90), false)
+        dxDrawLine(430 - 1, 431 - 1, 430 - 1, 553, tocolor(62, 62, 62, 108), 1, false)
+        dxDrawLine(567, 431 - 1, 430 - 1, 431 - 1, tocolor(62, 62, 62, 108), 1, false)
+        dxDrawLine(430 - 1, 553, 567, 553, tocolor(62, 62, 62, 108), 1, false)
+        dxDrawLine(567, 553, 567, 431 - 1, tocolor(62, 62, 62, 108), 1, false)
+        dxDrawRectangle(430, 431, 137, 122, tocolor(85, 85, 85, 90), false)
+        dxDrawLine(772 - 1, 273 - 1, 772 - 1, 395, tocolor(62, 62, 62, 108), 1, false)
+        dxDrawLine(909, 273 - 1, 772 - 1, 273 - 1, tocolor(62, 62, 62, 108), 1, false)
+        dxDrawLine(772 - 1, 395, 909, 395, tocolor(62, 62, 62, 108), 1, false)
+        dxDrawLine(909, 395, 909, 273 - 1, tocolor(62, 62, 62, 108), 1, false)
+        dxDrawRectangle(772, 273, 137, 122, tocolor(85, 85, 85, 90), false)
+        dxDrawLine(599 - 1, 273 - 1, 599 - 1, 395, tocolor(62, 62, 62, 108), 1, false)
+        dxDrawLine(736, 273 - 1, 599 - 1, 273 - 1, tocolor(62, 62, 62, 108), 1, false)
+        dxDrawLine(599 - 1, 395, 736, 395, tocolor(62, 62, 62, 108), 1, false)
+        dxDrawLine(736, 395, 736, 273 - 1, tocolor(62, 62, 62, 108), 1, false)
+        dxDrawRectangle(599, 273, 137, 122, tocolor(85, 85, 85, 90), false)
+        dxDrawImage(772, 430, 139, 123, ":coding/img/8.png", 0, 0, 0, tocolor(255, 255, 255, 255), false)
+        dxDrawImage(597, 430, 139, 123, ":coding/img/11.png", 0, 0, 0, tocolor(255, 255, 255, 255), false)
+        dxDrawImage(428, 430, 138, 121, ":coding/img/12.png", 0, 0, 0, tocolor(255, 255, 255, 255), false)
+        dxDrawImage(772, 272, 137, 120, ":coding/img/13.png", 0, 0, 0, tocolor(255, 255, 255, 255), false)
+        dxDrawImage(599, 271, 137, 124, ":coding/img/14.png", 0, 0, 0, tocolor(255, 255, 255, 255), false)
+        dxDrawLine(428 - 1, 273 - 1, 428 - 1, 395, tocolor(62, 62, 62, 90), 1, false)
+        dxDrawLine(565, 273 - 1, 428 - 1, 273 - 1, tocolor(62, 62, 62, 90), 1, false)
+        dxDrawLine(428 - 1, 395, 565, 395, tocolor(62, 62, 62, 90), 1, false)
+        dxDrawLine(565, 395, 565, 273 - 1, tocolor(62, 62, 62, 90), 1, false)
+        dxDrawRectangle(428, 273, 137, 122, tocolor(85, 85, 85, 90), false)
+        dxDrawImage(428, 271, 139, 124, ":coding/img/1.png", 0, 0, 0, tocolor(255, 255, 255, 255), false)
+        dxDrawLine(567 - 1, 597 - 1, 567 - 1, 681, tocolor(51, 51, 51, 108), 1, false)
+        --dxDrawLine(772, 597 - 1, 567 - 1, 597 - 1, tocolor(51, 51, 51, 108), 1, false)
+        --dxDrawLine(567 - 1, 681, 772, 681, tocolor(51, 51, 51, 108), 1, false)
+        --dxDrawLine(772, 681, 772, 597 - 1, tocolor(51, 51, 51, 108), 1, false)
+        --dxDrawRectangle(567, 597, 205, 84, tocolor(51, 51, 51, 108), false)
+        --dxDrawText("Buy", 568, 598, 772, 681, tocolor(4, 101, 0, 206), 3.00, "pricedown", "center", "center", false, false, false, false, false)
+        dxDrawRectangle(886, 203, 23, 21, tocolor(87, 0, 0, 206), false)
+        dxDrawText("X", 887, 204, 908, 224, tocolor(255, 255, 255, 255), 1.00, "default-bold", "center", "center", false, false, false, false, false)
+        dxDrawLine(429 - 1, 402 - 1, 429 - 1, 421, tocolor(43, 43, 43, 161), 1, false)
+        dxDrawLine(908, 402 - 1, 429 - 1, 402 - 1, tocolor(43, 43, 43, 161), 1, false)
+        dxDrawLine(429 - 1, 421, 908, 421, tocolor(43, 43, 43, 161), 1, false)
+        dxDrawLine(908, 421, 908, 402 - 1, tocolor(43, 43, 43, 161), 1, false)
+        dxDrawRectangle(429, 402, 479, 19, tocolor(74, 74, 74, 161), false)
+        dxDrawLine(429 - 1, 562 - 1, 429 - 1, 581, tocolor(43, 43, 43, 161), 1, false)
+        dxDrawLine(908, 562 - 1, 429 - 1, 562 - 1, tocolor(43, 43, 43, 161), 1, false)
+        dxDrawLine(429 - 1, 581, 908, 581, tocolor(43, 43, 43, 161), 1, false)
+        dxDrawLine(908, 581, 908, 562 - 1, tocolor(43, 43, 43, 161), 1, false)
+        dxDrawRectangle(429, 562, 479, 19, tocolor(74, 74, 74, 161), false)
+        dxDrawText("8$", 432, 403, 563, 421, tocolor(255, 255, 255, 255), 1.00, "default-bold", "center", "top", false, false, false, false, false)
+        dxDrawText("5$", 600, 403, 731, 421, tocolor(255, 255, 255, 255), 1.00, "default-bold", "center", "top", false, false, false, false, false)
+        dxDrawText("3$", 777, 402, 908, 420, tocolor(255, 255, 255, 255), 1.00, "default-bold", "center", "top", false, false, false, false, false)
+        dxDrawText("15$", 429, 563, 560, 581, tocolor(255, 255, 255, 255), 1.00, "default-bold", "center", "top", false, false, false, false, false)
+        dxDrawText("13$", 599, 564, 730, 582, tocolor(255, 255, 255, 255), 1.00, "default-bold", "center", "top", false, false, false, false, false)
+        dxDrawText("7$", 777, 563, 908, 581, tocolor(255, 255, 255, 255), 1.00, "default-bold", "center", "top", false, false, false, false, false)
+        dxDrawLine(754, 563, 754, 580, tocolor(255, 255, 255, 255), 1, false)
+        dxDrawLine(577, 562, 577, 579, tocolor(255, 255, 255, 255), 1, false)
+        dxDrawLine(578, 403, 578, 420, tocolor(255, 255, 255, 255), 1, false)
+        dxDrawLine(753, 402, 753, 419, tocolor(255, 255, 255, 255), 1, false)
+end
+
+function dXpanel ()
+  			local showed = not showed
+  		if showed then
+  			addEventHandler("onClientRender",root,dxbutton)
+  			showCursor ( true )
+  		end
+end
+addEvent("onHitMarkerShowDxPanel",true)
+addEventHandler("onHitMarkerShowDxPanel",localPlayer,dXpanel)
+--bindKey ("F7","down", dXpanel)
+
+function Closing ()
+  if (source==Close) then
+    removeEventHandler("onClientRender", root, dxbutton)
+		showCursor ( false )
+  end
+end
+addEventHandler ("onClientGUIClick",root,Closing)
+
+function buttonFood1 ()
+ if (source==b1)then
+      setPedAnimation( localPlayer, "vending", "vend_use" )
+       triggerServerEvent ( "takeMoneysB1", localPlayer)
+    setTimer(function()
+        setPedAnimation(localPlayer)
+    end, 2600, 0)
+  end
+end
+addEventHandler ("onClientGUIClick",root,buttonFood1)
+
+function buttonFood2 ()
+ if (source==b2)then
+    setPedAnimation( localPlayer, "vending", "vend_use" )
+       triggerServerEvent ( "takeMoneysB2", localPlayer)
+    setTimer(function()
+        setPedAnimation(localPlayer)
+    end, 2600, 0)
+  end
+end
+addEventHandler ("onClientGUIClick",root,buttonFood2)
+
+function buttonFood3 ()
+ if (source==b3)then
+      setPedAnimation( localPlayer, "vending", "vend_use" )
+       triggerServerEvent ( "takeMoneysB3", localPlayer)
+    setTimer(function()
+        setPedAnimation(localPlayer)
+    end, 2600, 0)
+  end
+end
+addEventHandler ("onClientGUIClick",root,buttonFood3)
+
+function buttonFood4 ()
+ if (source==b4)then
+      setPedAnimation( localPlayer, "vending", "vend_use" )
+       triggerServerEvent ( "takeMoneysB4", localPlayer)
+    setTimer(function()
+        setPedAnimation(localPlayer)
+    end, 2600, 0)
+  end
+end
+addEventHandler ("onClientGUIClick",root,buttonFood4)
+
+function buttonFood5 ()
+ if (source==b5)then
+      setPedAnimation( localPlayer, "vending", "vend_use" )
+       triggerServerEvent ( "takeMoneysB5", localPlayer)
+    setTimer(function()
+        setPedAnimation(localPlayer)
+    end, 2600, 0)
+  end
+end
+addEventHandler ("onClientGUIClick",root,buttonFood5)
+
+function buttonFood6 ()
+ if (source==b6)then
+  setPedAnimation( localPlayer, "vending", "vend_use" )
+    triggerServerEvent ( "takeMoneysB6", localPlayer)
+    setTimer(function()
+        setPedAnimation(localPlayer)
+    end, 2600, 0)
+
+  end
+end
+addEventHandler ("onClientGUIClick",root,buttonFood6)
---
+++ server.lua
@@ -0,0 +1,186 @@
+--[[
+██╗====██╗=█████╗=██╗=====██╗██████╗=====██████╗=███████╗██╗===██╗
+██║====██║██╔══██╗██║=====██║██╔══██╗====██╔══██╗██╔════╝██║===██║
+██║=█╗=██║███████║██║=====██║██║==██║====██║==██║█████╗==██║===██║
+██║███╗██║██╔══██║██║=====██║██║==██║====██║==██║██╔══╝==╚██╗=██╔╝
+╚███╔███╔╝██║==██║███████╗██║██████╔╝====██████╔╝███████╗=╚████╔╝=
+=╚══╝╚══╝=╚═╝==╚═╝╚══════╝╚═╝╚═════╝=====╚═════╝=╚══════╝==╚═══╝==
+==================================================================
+command list (
+--addvending [create new vending machine]-"TrialAdmin"
+--showvending [show all the vending machine in server]-"TrialAdmin"
+--delvending [remove vending machine ID]-"TrialAdmin"
+--delallvending [ remove all vinding machine in the server]-"LeadScripter"
+)
+--]]
+
+
+maxe = 1
+mysql = exports.mysql
+local allMarkers = createElement("machinesMarker")
+
+mysql:query ( "CREATE TABLE IF NOT EXISTS machins ( ID INT AUTO_INCREMENT KEY,x INT,y INT,z INT,rotation INT,dimension INT,interior INT)"  ); 
+addCommandHandler("addvending", function(thePlayer, commandName, id)
+    local rank = exports.integration
+    if rank:isPlayerTrialAdmin(thePlayer) then
+        local dim = getElementDimension(thePlayer)
+        local int = getElementInterior(thePlayer)
+        local _, _, rot = getElementRotation(thePlayer)
+        local objectPosition = thePlayer.matrix.position + thePlayer.matrix.forward * 1.5
+        local x, y, z = objectPosition:getX(), objectPosition:getY(), objectPosition:getZ()
+        local adminUsername = getElementData(thePlayer, "account:username")
+        local adminTitle = exports.global:getPlayerAdminTitle(thePlayer)
+        local objectMachineFood = createObject(1776, x, y, z, 0, 0, rot-180)
+        local query = mysql:query_free("INSERT INTO machins (x,y,z,rotation,dimension,interior) VALUES ('"..x.."','"..y.."',' "..z.."',' "..rot.."',' "..dim.."',' "..int.."')")
+        if query then
+          query = mysql:query("SELECT ID FROM machins ORDER BY ID DESC LIMIT 1")
+            if query then
+                while true do
+                    local row = mysql:fetch_assoc(query)
+                    if not row then break end
+                    exports.global:sendMessageToAdmins("[Vending]: "..adminTitle.." "..getPlayerName(thePlayer):gsub("_", " ").." ("..adminUsername..") has create new vending machin #"..row["ID"]..".")
+                    outputChatBox("vending machine created with ID #"..row["ID"].."!", thePlayer, 0, 255, 0)
+                end
+            end
+        end
+    end
+end)
+
+
+
+
+addEvent("takeMoneysB1",true)
+				addEventHandler("takeMoneysB1",root,function ()
+			exports.global:takeMoney(client,8)
+      exports.global:giveItem(source, 1, maxe)
+end)
+
+addEvent("takeMoneysB2",true)
+				addEventHandler("takeMoneysB2",root,function ()
+			exports.global:takeMoney(client,5)
+      exports.global:giveItem(source, 14, maxe)
+end)
+
+addEvent("takeMoneysB3",true)
+				addEventHandler("takeMoneysB3",root,function ()
+			exports.global:takeMoney(client,3)
+      exports.global:giveItem(source, 13, maxe)
+end)
+
+addEvent("takeMoneysB4",true)
+				addEventHandler("takeMoneysB4",root,function ()
+			exports.global:takeMoney(client,15)
+      exports.global:giveItem(source,12, maxe)
+end)
+
+addEvent("takeMoneysB5",true)
+				addEventHandler("takeMoneysB5",root,function ()
+			exports.global:takeMoney(client,13)
+      exports.global:giveItem(source, 11, maxe)
+end)
+
+addEvent("takeMoneysB6",true)
+        addEventHandler("takeMoneysB6",root,function ( thePlayer, itemID, itemValue)
+            exports.global:takeMoney(client,7)
+	          exports.global:giveItem(source, 8, maxe)
+end)
+
+function loadSQL()
+    local query = mysql:query("SELECT  * FROM machins")
+    if query then
+        while true do
+        local row = mysql:fetch_assoc(query)
+        if not row then break end
+        local x = tonumber(row["x"])
+        local y = tonumber(row["y"])
+        local z = tonumber(row["z"])
+        local rot = tonumber(row["rotation"]) 
+        local dim = tonumber(row["dimension"])
+        local int = tonumber(row["interior"])
+        local objectMachineFood = createObject(1776, x, y, z, 0, 0, rot)
+        local theMarker = createMarker(objectMachineFood.matrix.position + objectMachinFood.matrix.right * 0.3 - objectMachinFood.matrix.forward * 1, "cylinder", 0.30000001, 255, 255, 0, 0)
+        setElementParent(theMarker, allMarkers)
+        setElementDimension(objectMachineFood, dim)
+        setElementInterior(objectMachineFood, int)
+        setElementDimension(theMarker, dim)
+        setElementInterior(theMarker, int)
+    end
+  end
+end
+addEventHandler("onResourceStart", resourceRoot, loadSQL)
+
+function MarkerHit( thePlayer,hitElement)
+    triggerClientEvent (thePlayer,  "onHitMarkerShowDxPanel", thePlayer)
+end
+addEventHandler( "onMarkerHit", allMarkers, MarkerHit )
+
+
+addCommandHandler("showvending",function (thePlayer,commandName)
+  local rank = exports.integration
+  if (rank:isPlayerTrialAdmin(thePlayer)) then
+                local sqlshow = mysql:query ( "SELECT ID FROM machins")
+                 if (sqlshow) then
+            local continue = true
+            while continue do
+               local row = mysql:fetch_assoc(sqlshow)
+                if not row then break end
+
+            outputChatBox("All vending Mashin in the server IDS #" .. row["ID"] .. "!", thePlayer, 0, 255, 0)
+          end
+      end
+    end
+end)
+
+
+addCommandHandler("delvending",function (thePlayer,_,id)
+    local rank = exports.integration
+      if (rank:isPlayerTrialAdmin(thePlayer)) then
+  local adminUsername = getElementData(thePlayer, "account:username")
+  local adminTitle = exports.global:getPlayerAdminTitle(thePlayer)
+        local id = tonumber(id)
+	if id then
+        		local query = mysql:query_free("DELETE FROM machins WHERE ID='" .. id .. "'")
+		if query then
+      			  exports.global:sendMessageToAdmins("[Vending]: "..adminTitle.." ".. getPlayerName(thePlayer):gsub("_", " ").." ("..adminUsername..") has removed the vending machin #".. id ..".")
+		end
+		return query
+    	end
+			outputChatBox("SYNTAX: /delvending [ID]", thePlayer, 255, 194, 14)
+    end
+end)
+
+
+addCommandHandler("delallvending",function (thePlayer,commandName)
+    local rank = exports.integration
+    if (rank:isPlayerLeadScripter(thePlayer)) then
+  local adminUsername = getElementData(thePlayer, "account:username")
+  local adminTitle = exports.global:getPlayerAdminTitle(thePlayer)
+                local delAllvend = mysql:query ( "DELETE FROM machins WHERE ID")
+                 if (delAllvend) then
+            local continue = true
+            while continue do
+               local row = mysql:fetch_assoc(delAllvend)
+                  exports.global:sendMessageToAdmins("[Vending]: "..adminTitle.." ".. getPlayerName(thePlayer):gsub("_", " ").." ("..adminUsername..") has removed all the vending machins #")
+                  executeCommandHandler ( "restartres coding", thePlayer )
+                if not row then break end
+          end
+      end
+    end
+end)
+
+function restartResCommand (thePlayer)  
+      local rank = exports.integration
+      if (rank:isPlayerTrialAdmin(thePlayer)) then
+        restartResource(getThisResource())
+    end
+  end
+addCommandHandler ("addvending",restartResCommand)
+addCommandHandler ("delvending",restartResCommand)
+
+function restartResCommandDev (thePlayer)  
+      local rank = exports.integration
+      if (rank:isPlayerLeadScripter(thePlayer)) then
+        restartResource(getThisResource())
+    end
+  end
+addCommandHandler ("delallvending",restartResCommandDev)
